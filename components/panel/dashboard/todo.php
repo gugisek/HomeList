58a7d48dc63528dbe3f1b0972574d773649d389e
gugisek <gugisek@gmail.com>
2025-04-14 18:16:33 +0200

    fix archive

--- components/panel/dashboard/todo.php
+++ components/panel/dashboard/todo.php
@@ -107,6 +107,20 @@ if ($list == "archive"){
                   </div>
                 </div>
               </div>';
+      }else{
+        echo '
+                            <li class="flex ictems-center justify-center py-4 h-full">
+                              <div class="text-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto h-12 w-12 text-green-400">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                                </svg>
+                                <h3 class="mt-2 text-sm font-semibold text-gray-900">Brak zadań w archiwum</h3>
+                                <p class="mt-1 text-sm text-gray-500">Trafiają tu zadania po jednym dniu od daty ich wykonania.</p>
+                              </div>
+    
+                            </li>
+                        ';
+  
       }
       
     }else{
