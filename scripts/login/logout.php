df4688266136b46adf3413b22dee1944f4519c7f
gugisek <gugisek@gmail.com>
2025-04-21 13:16:31 +0200

    fix conn

--- scripts/login/logout.php
+++ scripts/login/logout.php
@@ -2,10 +2,9 @@
 session_start();
 session_unset();
 session_destroy();
-
 // Usuń remember_me token
 if (isset($_COOKIE['remember_me'])) {
-    include '../../conn_db.php';
+    include '../conn_db.php';
     [$selector] = explode(':', $_COOKIE['remember_me']);
     $stmt = $conn->prepare("DELETE FROM remember_tokens WHERE selector = ?");
     $stmt->bind_param("s", $selector);
