17a15405a0118eeb57995a09a4f7653a64fba50f
gugisek <gugisek@gmail.com>
2025-10-28 12:29:10 +0100

    hot_fix

--- scripts/login/register_script.php
+++ scripts/login/register_script.php
@@ -46,7 +46,8 @@ if ($login_sha != "" || $password_sha != "" || $name != "") {
                 $object_type="List";
                 $action_type = '2';
                 $desc = 'Dodano listę';
-                include "../log.php";
+                $sql_log = "INSERT INTO `logs` (`user_id`, `when`, `object_id`, `object_type`, `before`, `after`, `type`, `description`) VALUES ('$owner_id', current_timestamp(), '$object_id', '$object_type', '$before', '$after', '$action_type', '$desc')";
+                mysqli_query($conn, $sql_log);
                 //log
 
             }     
